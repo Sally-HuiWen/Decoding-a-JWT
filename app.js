aa3fdba03738ada27ccfe0c48efc0371f1743668
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-04-05 23:23:18 -0700

    complete

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 require('dotenv').config();
 
 // Import package
-
 // Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
@@ -11,7 +11,10 @@ require('dotenv').config();
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
 // Your code here
-
+// secret= require('crypto').randomBytes(64).toString('hex')
+// console.log(secret);//8b0865c60a7e9a96ed6281162cde35ee5d056ed28e9e8fb45589a1620b62209d73afd625fd7689d6829db46e9364e04f34c7627356c3e7c3b5639882b44ee169
+let myPayLoad = {email: 'brightlifewenwen@gmail.com', name: 'Sally'}
+token = jwt.sign(myPayLoad, process.env.SECRET_KEY, {expiresIn: '1s'})
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
 
@@ -20,6 +23,7 @@ console.log('JWT:', token);
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
 // Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -29,9 +33,9 @@ console.log('Payload:', payload);
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
 // Your code here
-
+    verifiedPayload = jwt.verify(token,process.env.SECRET_KEY)
 // See the verified payload in the console - DO NOT MODIFY
-console.log('Verified Payload:', verifiedPayload);
+    console.log('Verified Payload:', verifiedPayload);
 
 // (Optional) Bonus: Catch Error With Invalid Signature
 // Generate an alternate secret key and use it
